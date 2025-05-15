9d690cc84163732050ba193d4340decc48ae4c7a
Stepan Malov <moonsightofficial@gmail.com>
2025-05-15 14:02:53 +0300

    -

--- src/main/java/com/example/emp_control_system/EmpControlSystemApplication.java
+++ src/main/java/com/example/emp_control_system/EmpControlSystemApplication.java
@@ -1,14 +1,7 @@
 package com.example.emp_control_system;
 
-import com.example.emp_control_system.Entities.Department;
-import com.example.emp_control_system.Entities.Employee;
-import com.example.emp_control_system.Entities.Status;
-import com.example.emp_control_system.Repository.DepartmentRepository;
-import com.example.emp_control_system.Repository.EmployeeRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmpControlSystemApplication {
