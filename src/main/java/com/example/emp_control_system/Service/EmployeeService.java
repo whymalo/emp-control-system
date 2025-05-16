43e950957b29d39c765583451c4606457ea34831
Stepan Malov <moonsightofficial@gmail.com>
2025-05-16 19:49:42 +0300

    Добавлен поиск сотрудника по ключевому слову

--- src/main/java/com/example/emp_control_system/Service/EmployeeService.java
+++ src/main/java/com/example/emp_control_system/Service/EmployeeService.java
@@ -4,6 +4,7 @@ import com.example.emp_control_system.Entities.Employee;
 import com.example.emp_control_system.Repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,5 +23,18 @@ public class EmployeeService {
     public Optional<Employee> getById(int id) {
         return employeeRepository.findById(id);
     }
+
+    public List<Employee> searchByKeyword(String keyword) {
+        List<Employee> result = new ArrayList<>();
+        try{
+            int id = Integer.parseInt(keyword);
+            employeeRepository.findById(id).ifPresent(result::add);
+        }catch (NumberFormatException ignored){}
+        result.addAll(employeeRepository.findByNameContainingIgnoreCaseOrSurnameContainingIgnoreCaseOrEmailContainingIgnoreCase(
+                keyword, keyword, keyword
+        ));
+        return result.stream().distinct().toList();
+    }
+
 
 }
