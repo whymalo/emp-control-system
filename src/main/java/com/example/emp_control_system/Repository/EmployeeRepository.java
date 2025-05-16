041c3be6b114e2f7c3ea6b0043be24faf99c2eaf
Stepan Malov <moonsightofficial@gmail.com>
2025-05-16 19:51:45 +0300

    .

--- src/main/java/com/example/emp_control_system/Repository/EmployeeRepository.java
+++ src/main/java/com/example/emp_control_system/Repository/EmployeeRepository.java
@@ -3,5 +3,10 @@ package com.example.emp_control_system.Repository;
 import com.example.emp_control_system.Entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findByNameContainingIgnoreCaseOrSurnameContainingIgnoreCaseOrEmailContainingIgnoreCase(
+            String name, String surname, String email
+    );
 }
