6299e051cd9fbe52ea394e711ee70d7c3f45142b
Stepan Malov <moonsightofficial@gmail.com>
2025-05-19 19:43:07 +0300

    dto для формы добавления сотрудника

---
+++ src/main/java/com/example/emp_control_system/dto/EmployeeForm.java
@@ -0,0 +1,49 @@
+package com.example.emp_control_system.dto;
+
+public class EmployeeForm {
+    private String name;
+    private String surname;
+    private String email;
+    private Integer departmentId;
+    private Integer salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
